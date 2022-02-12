9c20a22a0d750343bb3ea94d470c3f3d604a0232
Jingqi Duan <37085905+JingqiDuan@users.noreply.github.com>
2022-02-13 01:46:30 +0800

    Create 34.FindFirstandLastPositionofElementinSortedArray.java

---
+++ 34.FindFirstandLastPositionofElementinSortedArray.java
@@ -0,0 +1,46 @@
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        if(nums == null || nums.length == 0){
+            return new int[]{-1, -1};
+        }
+         int firstPosition = findFirstPosition(nums, target);
+         if(firstPosition == -1){
+             return new int[]{-1, -1};
+         }
+         int lastPosition = findLastPosition(nums, target);
+         return new int[]{firstPosition, lastPosition};
+    }
+    private int findFirstPosition(int[] nums, int target){
+        int left = 0, right = nums.length -1;
+        while(left < right){
+            int mid = left + (right - left)/2;
+            if(nums[mid] < target){
+                left = mid + 1;
+            }else if(nums[mid] == target){
+                right = mid;
+            }else{
+                right = mid -1;
+            }
+        }
+        if(nums[left] == target){
+            return left;
+        }else{
+            return -1;
+        }
+    }
+    private int findLastPosition(int[] nums, int target){
+        int left = 0, right = nums.length -1;
+        while(left < right){
+            //round up when calculate the mid for lastPo
+            int mid = left + (right - left +1)/2;
+            if(nums[mid] < target){
+                left = mid + 1;
+            }else if(nums[mid] == target){
+                left = mid;
+            }else{
+                right = mid -1;
+            }
+        }
+        return right;
+    }
+}
