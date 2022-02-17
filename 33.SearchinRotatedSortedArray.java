f4086a1b892b98681aea6fa7d8e406af48fca87d
Jingqi Duan <37085905+JingqiDuan@users.noreply.github.com>
2022-02-17 11:47:01 +0800

    Update 33.SearchinRotatedSortedArray.java

--- 33.SearchinRotatedSortedArray.java
+++ 33.SearchinRotatedSortedArray.java
@@ -1,3 +1,36 @@
+/*class Solution {
+    public int search(int[] nums, int target) {
+        if(nums.length == 0 || nums == null){
+            return -1;
+        }
+        int left = 0, right = nums.length -1;
+        while(left <= right){
+            int mid = left + (right - left) / 2;
+            if(nums[mid] == target){
+                return mid;
+            }
+            if(nums[left] < nums[mid]){ // left ordering
+                if(target < nums[mid] && nums[left] <= target){//search in left part
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
+            }else{ //right ordering
+            //when element number is 2 and not inorder
+                if(nums[right] ==  target){
+                    return right;
+                }
+                if(nums[mid] < target && target <= nums[right]){
+                    left = mid + 1;
+                }else{
+                    right = mid - 1;
+                }
+            }
+        }
+        return -1;
+    }
+}*/
+
 class Solution {
     public int search(int[] nums, int target) {
         if(nums.length == 0 || nums == null){
