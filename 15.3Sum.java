7b00fcff554cc33bec6910c15649f6dae5e3115a
Jingqi Duan <37085905+JingqiDuan@users.noreply.github.com>
2022-03-08 20:37:44 +0800

    Update 15.3Sum.java

--- 15.3Sum.java
+++ 15.3Sum.java
@@ -5,6 +5,51 @@ class Solution {
             return results;
         }
         Arrays.sort(nums);
+        //a+b+c=0 => a=-(b+c)
+        for(int i = 0; i < nums.length; i++){
+            if(i > 0 && nums[i] == nums[i - 1]){
+                continue;
+            }
+            findTwoSum(nums, i, results);
+        }
+        return results;
+    }
+
+    private void findTwoSum(int[] nums, int index, List<List<Integer>> results){
+        int target = -nums[index];
+        int left = index + 1;
+        int right = nums.length - 1;
+        for(int i = index; i < nums.length - 1; i++){
+            while(left < right){
+                if(nums[left] < target - nums[right]){
+                    left++;
+                }else if(nums[left] > target - nums[right]){
+                    right--;
+                }else{
+                    ArrayList<Integer> triple = new ArrayList<>();
+                    triple.add(nums[index]);
+                    triple.add(nums[left]);
+                    triple.add(nums[right]);
+                    results.add(triple);
+                    left++;
+                    right--;
+                    while(left < right && nums[left] == nums[left - 1]){
+                        left++;
+                    }
+                }
+            }
+        }
+    }
+    
+}
+
+/* class Solution {
+    public List<List<Integer>> threeSum(int[] nums) {
+        List<List<Integer>> results = new ArrayList<>();
+        if(nums == null || nums.length == 0){
+            return results;
+        }
+        Arrays.sort(nums);
         Stack<Integer> path = new Stack<>();
         helper(nums, 0, path, 0, results);
         return results;
@@ -24,3 +69,4 @@ class Solution {
         }
     }
 }
+*/
