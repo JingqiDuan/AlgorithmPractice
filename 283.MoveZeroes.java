6ec34626a30fb39eba386e196dc65b8c7ad562c7
Jingqi Duan <37085905+JingqiDuan@users.noreply.github.com>
2022-03-10 01:12:42 +0800

    Create 283.MoveZeroes.java

---
+++ 283.MoveZeroes.java
@@ -0,0 +1,18 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left = 0, right = 0;
+        while(right < nums.length){
+            if(nums[right] != 0){
+                nums[left] = nums[right];
+                left++;
+                right++;
+            }else{
+                right++;
+            }
+        }
+        while(left < nums.length){
+            nums[left] = 0;
+            left++;
+        }
+    }
+}
