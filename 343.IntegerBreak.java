3201275bd61e6bd68c5013aaca0191f08ce09af0
Jingqi Duan <37085905+JingqiDuan@users.noreply.github.com>
2022-04-29 13:58:47 +0800

    Create 343.IntegerBreak.java

---
+++ 343.IntegerBreak.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int integerBreak(int n) {
+        if(n < 4){
+            return n - 1;
+        }
+        int prod = 1;
+        while(n > 4){
+            prod *= 3;
+            n -= 3;
+        }
+        return prod * n;
+    }
+}
